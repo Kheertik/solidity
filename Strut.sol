ed03c688d0b3790380a742f23642c730fc7fe46f
Kheertik Jeetun <kjeetun@umail.utm.ac.mu>
2024-06-24 18:56:13 +0400

    First sol project

---
+++ Strut.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.7.0
+
+Contract Example {
+    Struct Car {
+        uint door;
+        uint engineSize;
+        uint wheels;
+        string color;
+        string 
+    }
+
+    Strut Carpet {
+        string name;
+        uint length;
+        unit width;
+        bool shipped;
+    }
+
+    
+}
